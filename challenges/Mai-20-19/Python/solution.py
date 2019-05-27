35563a7224a38613a780ed917de7cf962f7e0dbc
lucasgcb <lucascoliado@hotmail.com>
2019-05-27 15:32:58 +0200

    Add pair constructor solution

--- challenges/Mai-20-19/Python/solution.py
+++ challenges/Mai-20-19/Python/solution.py
@@ -1,2 +1,32 @@
-def foo(bar):
-    pass
+def cons(a, b):
+    ## Construct a higher order function that returns an input pair
+    def pair(f):
+        return f(a, b)
+    return pair
+
+def car(f):
+    # f will fold the values into the function that gets input.
+    # Return a function that takes two inputs and returns the first result.
+    return f(lambda a,b: a)
+
+def cdr(f):
+    # f will fold the values into the function that gets input.
+    # Return a function that takes two inputs and returns the first result.
+    return f(lambda a,b: b)
+
+
+### More readable solutions
+
+def car_alt(f):
+    def return_first_input(a,b):
+        return a
+    # f will fold the values into the function that gets input.
+    # Return a function that takes two inputs and returns the first result.
+    return f(return_first_input)
+
+def cdr_alt(f):
+    def return_second_input(a,b):
+        return b
+    # f will fold the values into the function that gets input.
+    # Return a function that takes two inputs and returns the first result.
+    return f(return_second_input)
--- challenges/Mai-20-19/Python/tests/test_input.py
+++ challenges/Mai-20-19/Python/tests/test_input.py
@@ -3,11 +3,31 @@ import pytest
 from context import solution
 
 
-def test_input():
-    returned = True
-    assert returned == True
+def test_cons():
+    a = 3
+    b = 4
+    def pair(f):
+        return f(a,b)
+    result = solution.cons(a,b)
+    assert isinstance(result,type(pair))
+
+def test_car():
+    expected = 3
+    result = solution.car(solution.cons(3,4))
+    assert result == expected
+
+def test_cdr():
+    expected = 4
+    result = solution.cdr(solution.cons(3,4))
+    assert result == expected
 
 
-def test_input2():
-    returned = False
-    assert returned == False
+def test_car_alt():
+    expected = 8
+    result = solution.car_alt(solution.cons(8,7))
+    assert result == expected
+
+def test_cdr_alt():
+    expected = 7
+    result = solution.cdr_alt(solution.cons(8,7))
+    assert result == expected
