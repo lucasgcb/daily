10931243f1817a360df527c2511929d5290357dc
lucasgcb <lucascoliado@hotmail.com>
2019-05-27 17:39:19 +0200

    Add staircase climber solution (very unga)

--- challenges/Mai-27-19/Python/solution.py
+++ challenges/Mai-27-19/Python/solution.py
@@ -1,2 +1,46 @@
-def foo(bar):
-    pass
+
+def first_solution(minimum_step,total_steps):
+    """
+    Find the first, longest combination utilizing the minimum_step.
+    """
+
+    solution = []
+    for _ in range(0,total_steps):
+        solution.append(minimum_step)
+    return tuple(solution)
+
+def potential_finder(max_solution_size, potential, potentials, possible_steps):
+    """
+    Find permutations that are potentially the desired solution,
+    since the maximum solution size is given by the minimum step.
+    """
+    from copy import deepcopy
+    if not(len(potential) > max_solution_size):
+        if len(potential) > 0:
+            potentials.append(potential)
+        for step in possible_steps:
+            next_potential = deepcopy(potential)
+            next_potential.append(step)
+            potential_finder(max_solution_size,next_potential,potentials,possible_steps)
+    return
+
+def solver(max_solution_size, possible_steps, total_steps):
+    """
+    Filter through solutions found in potential candidates (combinations that == number of steps).
+    """
+    potentials = []
+    ack =[]
+    potential_finder(max_solution_size, ack, potentials, possible_steps)
+    potentials = filter(lambda potential_solution:sum(potential_solution)==total_steps, potentials)
+    return set(tuple(row) for row in potentials)
+
+def climber_possibilities(total_steps):
+    """
+    Figure out the longest solution, generate permutations based on the longest solution,
+    then filter out the permutations that correspond to paths. Return the paths as tuples.
+    """
+    possible_steps = [1,2]
+    start_solution = first_solution(possible_steps[0],total_steps)
+    max_solution_size = len(start_solution)
+    head = solver(max_solution_size, possible_steps, total_steps)
+    return set(tuple(row) for row in head)
--- challenges/Mai-27-19/Python/tests/test_input.py
+++ challenges/Mai-27-19/Python/tests/test_input.py
@@ -3,9 +3,52 @@ import pytest
 from context import solution
 
 
+
+def test_potential_finder():
+    """
+    Find permutations that are potentially the desired solution,
+    since the maximum solution size is given by the minimum step.
+    """
+    potentials = []
+    possible_steps = [1,2]
+    max_solution_size = len([1,2])
+    expected = [(1,),(2,),(1,1),(1,2),(2,1),(2,2)]
+    solution.potential_finder(max_solution_size, [], potentials, possible_steps)
+    potentialsSet = set(tuple(row) for row in potentials)
+    assert potentialsSet == set(expected)
+
+def test_solver():
+    """
+    Find permutations that are potentially the desired solution,
+    since the maximum solution size is given by the minimum step.
+    """
+    possible_steps = [1,3]
+    expected = [(1,1,1),(3,)]
+    max_solution_size = len([1,1,1])
+    total_steps = 3
+    result = solution.solver(max_solution_size,possible_steps,total_steps)
+    
+    assert set(result) == set(expected)
+
+def test_first_solution():
+    N = 4
+    minimum_step = 1
+    result = solution.first_solution(minimum_step, N)
+    expected = (1,1,1,1)
+    assert set(result) == set(expected)
+
 def test_input():
-    returned = True
-    assert returned == True
+    N = 4
+    results = solution.climber_possibilities(N)
+    expected = [ (1,1,1,1),
+                 (2,1,1),
+                 (1,2,1),
+                 (1,1,2),
+                 (2,2)]
+    results = set(results)
+    expected = set(expected)
+    assert expected==results
+    
 
 
 def test_input2():
